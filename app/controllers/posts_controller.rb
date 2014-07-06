3d4caa236b33f5ffb2890989b1e380f4f702b2d4
Patrick Jones <pmichaeljones@gmail.com>
2014-07-06 14:30:25 -0600

    added posts and comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
-class PostsController < ApplicationController
-  def index
+class PostsController < ActionController::Base
 
+  def index
+    @posts = Post.all
+    render :index, layout: "application"
   end
+
+  def show
+    @post = Post.find[params[:id]]
+  end
+
 end
