d60437717aefc8638131394ca79cf80976bd6a4b
Patrick Jones <pmichaeljones@gmail.com>
2014-07-25 15:12:08 -0600

    ajax the comments

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,29 +1,58 @@
 class VotesController < ApplicationController
   before_action :require_user
+  respond_to :html, :js
 
   def create
     #binding.pry
 
-    if Vote.where(voteable:Post.find(params[:id]), user_id:current_user.id).empty?
-      @vote = Vote.create(vote: params[:vote], voteable: Post.find(params[:id]), user_id: current_user.id)
-      redirect_to :back
-    else
-      flash[:error] = "You already voted on that."
-      redirect_to :back
+    @post = Post.find(params[:id])
+
+    #binding.pry
+
+    respond_to do |format|
+
+      format.html do
+        if Vote.where(voteable:@post, user_id:current_user.id).empty?
+          @vote = Vote.create(vote: params[:vote], voteable: @post, user_id: current_user.id)
+          redirect_to :back
+        else
+          flash[:error] = "You already voted on that."
+          redirect_to :back
+        end
+      end
+
+      format.js do
+      end
+
     end
+
 
   end
 
   def comment_vote
     #binding.pry
 
-    if Vote.where(voteable:Comment.find(params[:id]), user_id:current_user.id).empty?
-      @vote = Vote.create(vote: params[:vote], voteable: Comment.find(params[:id]), user_id: current_user.id)
-      redirect_to :back
-    else
-      flash[:error] = "You already voted on that."
-      redirect_to :back
+    respond_to do |format|
+      format.html do
+        if Vote.where(voteable:Comment.find(params[:id]), user_id:current_user.id).empty?
+          @vote = Vote.create(vote: params[:vote], voteable: Comment.find(params[:id]), user_id: current_user.id)
+          redirect_to :back
+        else
+          flash[:error] = "You already voted on that."
+          redirect_to :back
+        end
+      end
+
+      format.js do
+        # if Vote.where(voteable:Comment.find(params[:id]), user_id:current_user.id).empty?
+        #   @vote = Vote.create(vote: params[:vote], voteable: Comment.find(params[:id]), user_id: current_user.id)
+        # else
+        #   redirect_to :back
+        # end
+
+      end
     end
+
 
   end
 
