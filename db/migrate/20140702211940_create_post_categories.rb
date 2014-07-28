dca968294586645ba6fdb1d351c14e4cdfab2572
Patrick Jones <pmichaeljones@gmail.com>
2014-07-28 11:44:27 -0600

    fixed db

--- db/migrate/20140702211940_create_post_categories.rb
+++ db/migrate/20140702211940_create_post_categories.rb
@@ -1,8 +1,8 @@
 class CreatePostCategories < ActiveRecord::Migration
   def change
-    create_table :post_categories do |t|
-      t.integer :post_id, :user_id
-      t.timestamps
+    # create_table :post_categories do |t|
+    #   t.integer :post_id, :user_id
+    #   t.timestamps
     end
   end
 end
